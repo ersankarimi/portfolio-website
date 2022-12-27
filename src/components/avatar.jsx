dd6818605fee927e6aa2379858cbeca726d9e7b5
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-27 14:33:39 +0800

    feat: create component

---
+++ src/components/avatar.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import avatar from "../../public/avatar.svg";
+
+export default function Avatar({ isLink, extraClassName }) {
+  if (isLink) {
+    return (
+      <Link href="/" className={`flex items-center gap-2 ${extraClassName}`}>
+        <Image src={avatar} alt="Ersan Avatar" />
+        <h1 className="font-bold ">Ersan</h1>
+      </Link>
+    );
+  }
+
+  return (
+    <div className={`flex items-center gap-2 ${extraClassName}`}>
+      <Image src={avatar} alt="Ersan Avatar" />
+      <h1 className="font-bold ">Ersan</h1>
+    </div>
+  );
+}
