e24d95cf0cd09ea9d27be2fe71587cfc6a80930a
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-27 14:18:56 +0800

    feat: add some elements

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import { MdMenu } from "react-icons/md";
+
+import avatar from "../../public/avatar.svg";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="px-6 py-3 ">
+      <nav className="relative flex items-center justify-between text-brand-foreground-500">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src={avatar} alt="Ersan Avatar" />
+          <h1 className="font-bold ">Ersan</h1>
+        </Link>
+        <MdMenu className="h-6 w-6 cursor-pointer" />
+      </nav>
+    </header>
+  );
 }
