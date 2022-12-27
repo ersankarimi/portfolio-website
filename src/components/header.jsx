6366486c412a25a40c9517dfd3412f5d579b501a
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-27 14:34:00 +0800

    refactor: changes the avatar element with Avatar component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
 import { MdMenu } from "react-icons/md";
 
-import avatar from "../../public/avatar.svg";
+import Avatar from "./avatar";
 
 export default function Header() {
   return (
     <header className="px-6 py-3 ">
       <nav className="relative flex items-center justify-between text-brand-foreground-500">
-        <Link href="/" className="flex items-center gap-2">
-          <Image src={avatar} alt="Ersan Avatar" />
-          <h1 className="font-bold ">Ersan</h1>
-        </Link>
+        <Avatar isLink />
         <MdMenu className="h-6 w-6 cursor-pointer" />
       </nav>
     </header>
