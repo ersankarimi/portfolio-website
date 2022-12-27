ee4df6907922895bcdcd0220f7568984d74a4544
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-27 10:42:02 +0800

    feat: setup the root layouts
    Import the Header and Footer component and setup them inside the layout component

--- src/components/root-layout.jsx
+++ src/components/root-layout.jsx
@@ -1,3 +1,12 @@
-export default function RootLayout() {
-  return <h1>Layout</h1>;
+import Footer from "./footer";
+import Header from "./header";
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
