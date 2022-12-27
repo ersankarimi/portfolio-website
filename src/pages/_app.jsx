11d276802f75c8997f2316b194d1693799eb1076
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-27 20:39:44 +0800

    refactor: changes the title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <RootLayout>
       <Head>
-        <title>Ersan Karimi Portfolio&apos;s</title>
+        <title>Ersan Karimi</title>
         <link rel="shortcut icon" href="/avatar.svg" />
       </Head>
       <Component {...pageProps} />
