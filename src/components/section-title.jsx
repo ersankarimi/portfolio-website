a33904c432e3cf4f6a1b7b7a349576c551d323a8
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-27 22:41:35 +0800

    refactor: update classes's style names

--- src/components/section-title.jsx
+++ src/components/section-title.jsx
@@ -1,11 +1,11 @@
 export default function SectionTitle({ title }) {
   return (
-    <div className="relative">
+    <div className="flex w-full items-center justify-center gap-4">
       <h2 className="text-xl font-bold text-brand-foreground-300">
         <span className="font-semibold text-purple-500">#</span>
         {title}
       </h2>
-      <div className="stroke-black">{title}</div>
+      <div className="h-[1px] w-full bg-brand-purple-500" />
     </div>
   );
 }
