539203e47f9c99317dee9fa11cbb695eb69c3010
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-28 21:07:07 +0800

    feat: import and re-export the ViewMoreButton component

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,11 @@ import ProjectCard from "./project-card";
 import RootLayout from "./root-layout";
 import SectionPage from "./section-page";
 import SectionTitle from "./section-title";
+import ViewMoreButton from "./view-more-button";
 
 export {
   RootLayout,
+  ViewMoreButton,
   PostCard,
   ProjectCard,
   BadgeSkill,
