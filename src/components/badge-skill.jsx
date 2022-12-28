e2d6781c8f3ff1d06ef3843522b10d0925cc038c
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-28 16:23:27 +0800

    feat: create component

---
+++ src/components/badge-skill.jsx
@@ -0,0 +1,38 @@
+import {
+  SiNextdotjs,
+  SiTailwindcss,
+  SiBootstrap,
+  SiFramer,
+  SiReactrouter,
+  SiJavascript,
+  SiGithub,
+  SiFigma,
+  SiHtml5,
+  SiCss3,
+  SiGit,
+  SiReact,
+} from "react-icons/si";
+
+export default function BadgeSkill({ skill }) {
+  const iconsObject = {
+    html: <SiHtml5 />,
+    css: <SiCss3 />,
+    javascript: <SiJavascript />,
+    react: <SiReact />,
+    reactrouter: <SiReactrouter />,
+    nextjs: <SiNextdotjs />,
+    git: <SiGit />,
+    github: <SiGithub />,
+    tailwindcss: <SiTailwindcss />,
+    bootstrap: <SiBootstrap />,
+    figma: <SiFigma />,
+    framermotion: <SiFramer />,
+  };
+
+  return (
+    <div className="flex cursor-default items-center gap-2 border-1 border-brand-foreground-600/60 bg-brand-selection-500/40 py-1 px-3 text-brand-purple-600 duration-150 hover:border-brand-foreground-600 hover:bg-brand-selection-500/80 hover:text-brand-purple-400">
+      {iconsObject[skill.toLowerCase().replace(/\W/, "")]}
+      {skill}
+    </div>
+  );
+}
