96dc0d1eb24fd217feae8c5ac69eaac8e1d22b1b
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-28 16:26:08 +0800

    feat: import and re-export the BadgeSkill component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Avatar from "./avatar";
+import BadgeSkill from "./badge-skill";
 import RootLayout from "./root-layout";
 import SectionPage from "./section-page";
 import SectionTitle from "./section-title";
 
-export { RootLayout, Avatar, SectionPage, SectionTitle };
+export { RootLayout, BadgeSkill, Avatar, SectionPage, SectionTitle };
