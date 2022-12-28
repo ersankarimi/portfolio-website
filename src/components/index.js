0694b9073522666e1c88db9b8a0ec5c8a7b463b1
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-28 19:08:12 +0800

    feat: import and re-export ProjectCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
 import Avatar from "./avatar";
 import BadgeSkill from "./badge-skill";
+import ProjectCard from "./project-card";
 import RootLayout from "./root-layout";
 import SectionPage from "./section-page";
 import SectionTitle from "./section-title";
 
-export { RootLayout, BadgeSkill, Avatar, SectionPage, SectionTitle };
+export {
+  RootLayout,
+  ProjectCard,
+  BadgeSkill,
+  Avatar,
+  SectionPage,
+  SectionTitle,
+};
