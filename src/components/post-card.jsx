ca50407a427aeaed022474560a54739d2954db6b
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-28 20:13:39 +0800

    feat: create component

---
+++ src/components/post-card.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+export default function PostCard({ title, description, link, year, type }) {
+  return (
+    <div className="flex flex-col border-1 border-brand-foreground-700">
+      <div className="border-b-1 border-current bg-brand-selection-600/60 px-4 py-1">
+        <h3 className="text-sm font-medium text-brand-foreground-600/90">
+          {type}
+        </h3>
+      </div>
+      <div className="flex flex-col gap-9 p-4">
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-xl font-bold">{title}</h1>
+            <h6 className="text-xs text-brand-comment-500">{year}</h6>
+          </div>
+          <p className="text-sm text-brand-foreground-700">{description}</p>
+        </div>
+
+        <Link
+          href={link}
+          className="w-max border-1 border-brand-foreground-700 px-4 py-2 text-brand-foreground-600 duration-200 hover:bg-brand-selection-500/50 hover:text-brand-cyan-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button
+            type="button"
+            className="flex items-center gap-2 font-light"
+            tabIndex={-1}
+          >
+            Check this out <MdKeyboardArrowRight className="h-5 w-5" />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
