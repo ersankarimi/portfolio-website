ba1b8fe00541221fe03f24e87136632cac86bd0f
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-28 21:51:54 +0800

    feat: add some contents

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,18 @@
+import Avatar from "./avatar";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex flex-col gap-12 border-t-1 border-t-brand-foreground-500 p-6">
+      <div className="flex flex-col gap-2">
+        <Avatar />
+        <p className="max-w-xs text-xs text-brand-foreground-600">
+          a Front-End Web Developer, Content Creator, and Student located in
+          Indonesia.
+        </p>
+      </div>
+      <p className="text-center text-sm font-semibold">
+        © 2022 Ersan Karimi. All Rights Reserved.
+      </p>
+    </footer>
+  );
 }
