2a5a93d0a8ddec0c98962159c936c4e93a1b98ee
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-29 11:03:49 +0800

    refactor: move the duration-200 classname to be a default class style of the component

--- src/components/redirect-link.jsx
+++ src/components/redirect-link.jsx
@@ -6,16 +6,19 @@ export default function RedirectLink({ children, type = "default", to }) {
   return (
     <Link
       href={to}
-      className={classNames("flex items-center gap-2 border-1 px-4 py-2", {
-        "text-sm font-light text-brand-foreground-600 duration-200 hover:bg-brand-selection-500/50 hover:text-brand-foreground-500 md:text-base":
-          type !== "view-more",
-        "border-brand-foreground-700 hover:border-brand-foreground-500 hover:text-cyan-400":
-          type === "default",
-        "border-brand-purple-700 hover:border-brand-purple-500 hover:text-brand-purple-400":
-          type === "demo",
-        "mx-auto my-4 border-brand-foreground-600 bg-brand-selection-600/60 hover:bg-brand-selection-600":
-          type === "view-more",
-      })}
+      className={classNames(
+        "flex items-center gap-2 border-1 px-4 py-2 duration-200",
+        {
+          "text-sm font-light text-brand-foreground-600 hover:bg-brand-selection-500/50 hover:text-brand-foreground-500 md:text-base":
+            type !== "view-more",
+          "border-brand-foreground-700 hover:border-brand-foreground-500 hover:text-cyan-400":
+            type === "default",
+          "border-brand-purple-700 hover:border-brand-purple-500 hover:text-brand-purple-400":
+            type === "demo",
+          "mx-auto my-4 border-brand-foreground-600 bg-brand-selection-600/60 hover:bg-brand-selection-600":
+            type === "view-more",
+        }
+      )}
     >
       {type === "view-more" ? (
         <>
