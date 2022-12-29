6d26a399e90a4a38bd5598833006f0ce0fa58667
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-29 14:50:20 +0800

    refactor: update bg class name while hovering

--- src/components/badge-skill.jsx
+++ src/components/badge-skill.jsx
@@ -30,7 +30,7 @@ export default function BadgeSkill({ skill }) {
   };
 
   return (
-    <div className="flex cursor-default items-center gap-2 border-1 border-brand-foreground-600/50 bg-brand-purple-500/[8%] py-1 px-3 text-brand-purple-400 duration-200 hover:border-brand-foreground-600/90 hover:bg-brand-purple-500/[15%] hover:text-brand-purple-300 hover:shadow-sm hover:shadow-brand-purple-500/70">
+    <div className="flex cursor-default items-center gap-2 border-1 border-brand-foreground-600/50 bg-brand-purple-500/[8%] py-1 px-3 text-brand-purple-400 duration-200 hover:border-brand-foreground-600/90 hover:bg-brand-purple-500/[20%] hover:text-brand-purple-300 hover:shadow-sm hover:shadow-brand-purple-500/70">
       {iconsObject[skill.toLowerCase().replace(/\W/, "")]}
       {skill}
     </div>
