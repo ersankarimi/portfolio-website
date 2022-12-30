4269644500fa7f78b63aea18c0f5ee12941e0213
Ersan Karimi <ersankarimii311@gmail.com>
2022-12-31 01:20:58 +0800

    feat: create a useMediaQuery custom hook
    This hook will return the boolean value. The boolean value is result of the given conditions on the query param.
    
    If the query param is matched with the current screen then return true, if not then return false

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    setMatches(mediaQueryList.matches);
+
+    const listener = (event) => {
+      setMatches(event.matches);
+    };
+
+    mediaQueryList.addEventListener("change", listener);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", listener);
+    };
+  }, [matches, query]);
+
+  return matches;
+}
